218253f45d0d120b62ee9e9ca066a479362118c6
Daniel <dcjones2015@gmail.com>
2019-09-25 12:12:24 -0700

    update

--- lab1/part1/schoolsearch.py
+++ lab1/part1/schoolsearch.py
@@ -1,6 +1,6 @@
 import sys
 
-# Student Class to 
+# Student Class to hold each student's data
 
 class Student:
     def __init__(self, student_data):
@@ -21,7 +21,7 @@ class Student:
     def __str__(self):
         return self.lastName + "," + self.firstName + "," + str(self.studentGrade) + "," + + str(self.classroomNum) + "," + str(self.busRouteNum) + "," + str(self.studentGPA) + "," + self.teacherLastName + "," + self.teacherLastName
 
-# Check Functions
+# Checker Functions
 
 def is_float(value):
     try:
@@ -48,9 +48,72 @@ def data_format_error(student_index):
     print("Error: Student data at index " + str(student_index) + " not formatted correctly")
 
 def missing_data_error():
-    print("Error: Could not locate students.txt")
+    print("Error: Could not find students.txt")
 
+# Commands for User 
 
+def print_help():
+    print("Available Commands:")
+    print("S[tudent]:  <lastname> [B[us]]")
+    print("T[eacher]:  <lastname>")
+    print("B[us]:  <number>")
+    print("G[rade]:  <number> [H[igh]|L[ow]]")
+    print("A[verage]:  <number>")
+    print("I[nfo]")
+    print("Q[uit]")
+
+def find_student(command_line, students):
+    if len(command_line) < 3:
+        bus_route = False
+    if len(command_line) > 3 or len(command_line) < 2:
+        syntax_error("S[tudent]  <lastname> [B[us]]")
+        return 1
+    else:
+        bus_route = command_line[2] == "B" 
+    
+
+# Command Functions
+
+def start(running, students):
+    command_line = input(">").split()
+    if(len(command_line) < 1):
+        print_help()
+        return 1
+    select = command_line[0]
+    select = select.split(":")[0]
+    if select[0] == "S":
+        command = find_student(command_line, students)
+        return command 
+    elif select[0] == "T":
+        command = find_teacher(command_line, students)
+        return command 
+    elif select[0] == "G":
+        command = find_grade(command_line, students)
+        return command 
+    elif select[0] == "B":
+        command = find_bus(command_line, students)
+        return command 
+    elif select[0] == "A":
+        command = find_average(command_line, students)
+        return command
+    elif select[0] == "I":
+        command = find_info(command_line, students)
+        return command 
+    elif select[0] == "Q":
+        print("Goodbye")
+        exit
+    else:
+        print_help()
+        return 1    
+
+def get_data(input_text):
+    all_student_data = []
+    for i in range(0, len(input_text) - 1):
+        student_info = Student(input_text[i])
+        if student_info.error:
+            return [-1, i]
+        all_student_data.append(student_info)
+    return all_student_data
 
 # Main Program 
 if __name__ == "__main__":
