b547631267857646d07aef5e2c485495016196e6
Daniel <dcjones2015@gmail.com>
2019-09-25 11:37:31 -0700

    Update

--- lab1/part1/schoolsearch.py
+++ lab1/part1/schoolsearch.py
@@ -1,5 +1,26 @@
 import sys
 
+# Student Class to 
+
+class Student:
+    def __init__(self, student_data):
+        split_line = student_data.split(",")
+        if not is_int(split_line[2]) or not is_int(split_line[3]) or not is_int(split_line[4]) or not is_float(split_line[5]) or not len(split_line) != 8:
+            self.error = True
+        else:
+            self.error = False
+            self.firstName = split_line[1]
+            self.lastName = split_line[0]
+            self.studentGrade = int(split_line[2])
+            self.classroomNum = int(split_line[3])
+            self.busRouteNum = int(split_line[4])
+            self.studentGPA = float(split_line[5])
+            self.teacherLastName = split_line[6]
+            self.teacherFirstName = split_line[7]
+        
+    def __str__(self):
+        return self.lastName + "," + self.firstName + "," + str(self.studentGrade) + "," + + str(self.classroomNum) + "," + str(self.busRouteNum) + "," + str(self.studentGPA) + "," + self.teacherLastName + "," + self.teacherLastName
+
 # Check Functions
 
 def is_float(value):
@@ -16,9 +37,9 @@ def is_int(val):
     except ValueError:
         return False
 
-def syntax_error(string):
+def syntax_error(invalid_string):
     print("Error: Invalid Syntax")
-    print(string)
+    print(invalid_string)
 
 def divide_by_zero_error():
     print("Error: No students in selected grade")
@@ -28,6 +49,8 @@ def data_format_error(student_index):
 
 def missing_data_error():
     print("Error: Could not locate students.txt")
+
+
 
 # Main Program 
 if __name__ == "__main__":
