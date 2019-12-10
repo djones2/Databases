d6d0d2e0c540d965eb2eb486a4e658cf617f8a50
Daniel <dcjones2015@gmail.com>
2019-12-10 12:48:11 -0800

    Create InnReservations.java

---
+++ lab7/InnReservations.java
@@ -0,0 +1,58 @@
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.LinkedHashMap;
+import java.time.LocalDate;
+import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InnReservations{
+   /* FR-1: Output a list of rooms to the user sorted by popularity. */
+   public static void roomsAndRates(){
+
+   }
+
+    /* FR-2: Allow a user to book a room. */
+   public static void reservations(){
+  
+   }
+
+    /* FR-3: Allow a user to make changes to a current reservation. */
+   public static void reservationsChange(){
+   
+   }
+
+    /* FR-4: Allow a user to cancel a reservation. */
+    public static void reservationCancellation(){
+   
+    }
+
+    /* FR-5: Allow a user to see specific details about a reservation. */
+    public static void detailedReservaion(){
+   
+    }
+
+    /* FR-6: See a month-by-month revenue of the entire year. */
+    public static void revenue(){
+   
+    }
+
+   /*  Display a menu for user. */
+   public static void mainMenu(){
+
+   }
+
+    /* Main program */
+   public static void main(){
+
+    }
+}
