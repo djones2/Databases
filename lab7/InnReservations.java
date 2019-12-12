0f90259338effb52a9ed68aaebb9190b6d57109f
bradenbug <bradenwicker@gmail.com>
2019-12-12 12:00:22 -0800

    made the README reflect reality

--- lab7/InnReservations.java
+++ lab7/InnReservations.java
@@ -217,6 +217,7 @@ public class InnReservations {
             }
         } catch (Exception ex) {
             System.out.println("Connection error. Please check your environment variables and try again.");
+            System.out.println(ex);
         }
     }
 }
